ef24f0857fd0c3e86003647937b377d450aa063e
ZinaGoreng <51062902+ZinaGoreng@users.noreply.github.com>
2022-08-21 13:45:30 +0700

    Create notif2.lua

---
+++ notif2.lua
@@ -0,0 +1,6 @@
+game.StarterGui:SetCore("SendNotification", {
+Title = "System"; -- the title (ofc)
+Text = "Changing Server"; -- what the text says (ofc)
+Icon = ""; -- the image if u want. 
+Duration = 2; -- how long the notification should in secounds
+})
