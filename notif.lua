a257f964edea21ebaaae8bc77b1ee38d17c8c193
ZinaGoreng <51062902+ZinaGoreng@users.noreply.github.com>
2022-08-21 13:41:13 +0700

    Create notif.lua

---
+++ notif.lua
@@ -0,0 +1,13 @@
+game.StarterGui:SetCore("SendNotification", {
+Title = "System"; -- the title (ofc)
+Text = "Script Activated"; -- what the text says (ofc)
+Icon = ""; -- the image if u want. 
+Duration = 2; -- how long the notification should in secounds
+})
+wait (.4)
+game.StarterGui:SetCore("SendNotification", {
+Title = "System"; -- the title (ofc)
+Text = "Changing Server"; -- what the text says (ofc)
+Icon = ""; -- the image if u want. 
+Duration = 2; -- how long the notification should in secounds
+})
