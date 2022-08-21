262f46db4142bf7f768a422a8f3befa08962d787
ZinaGoreng <51062902+ZinaGoreng@users.noreply.github.com>
2022-08-21 13:39:50 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,10 +1,4 @@
 print("Activated!")
-game.StarterGui:SetCore("SendNotification", {
-Title = "System"; -- the title (ofc)
-Text = "Script Activated"; -- what the text says (ofc)
-Icon = ""; -- the image if u want. 
-Duration = 2; -- how long the notification should in secounds
-})
 wait(7)
              for _, v in pairs(game.Workspace:GetDescendants()) do
                   if v:IsA("TouchTransmitter") or v:IsA("TouchInterest") then
